e23f93a3e59f740a71ec0ddc8edcf26c55c98ee4
hakuunabatata <emachadomartins1999@gmail.com>
2020-08-27 17:56:56 -0300

    create user model

--- server/src/database/migrations/1598556337289-CreateUsersTable.ts
+++ server/src/database/migrations/1598556337289-CreateUsersTable.ts
@@ -1,10 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm'
 
-export class CreateAppointments1598476792185 implements MigrationInterface {
+export default class CreateUsersTable1598556337289
+  implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,12 +15,17 @@ export class CreateAppointments1598476792185 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
---
+++ server/src/routes/users.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+import CreateUserService from '../services/CreateUserService'
+
+const usersRouter = Router()
+
+usersRouter.post('/', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+
+    const createUser = new CreateUserService()
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    })
+
+    const retUser = {
+      name,
+      email,
+    }
+
+    return res.json(retUser)
+  } catch (err) {
+    return res.status(400).json({ error: err.message })
+  }
+})
+
+export default usersRouter
